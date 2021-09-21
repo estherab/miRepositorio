3f52f3c3b1ecc783b75c1ebe410dc6e637469748
esthe <esthe@DESKTOP-VTFO8S3>
2021-09-21 19:23:49 +0200

    ESTHER 21/09/2021 - Añadido el enunciado

---
+++ Hoja1_ej11/src/paquete/Ejecuta.java
@@ -0,0 +1,14 @@
+package paquete;
+
+public class Ejecuta {
+
+	public static void main(String[] args) {
+		/*
+		 * 11. Programa que lee nombre y año de nacimiento de una serie de alumnos hasta introducir
+		   como nombre de alumno “fin”. En ese momento mostrará la edad media de los alumnos y
+		   el nombre del alumno menor. NOTA: Al introducir como nombre “fin”, no nos ha de pedir el
+		   año de nacimiento.
+		 */
+
+	}
+}
