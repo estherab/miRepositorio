4d19b3a05281463b448336b5de57dfeb6b7261f1
Esther Ávila Benito <esther.abenito@gmail.com>
2021-09-21 10:07:51 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-
+ 
 		for (int i = 1; i <= 10; i++) {
 			System.out.println("Tabla del " + i);
 			for (int j = 1; j <= 10; j++) {
