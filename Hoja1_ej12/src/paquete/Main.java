3ff0a4b0c532b8317ca1033d1200ab1b3b9509fc
esthe <esthe@DESKTOP-VTFO8S3>
2021-09-22 16:28:31 +0200

    ESTHER 22/09/2021 16:27 Hoja1_ej12 Main.java - Primera solución

---
+++ Hoja1_ej12/src/paquete/Main.java
@@ -0,0 +1,24 @@
+package paquete;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 Programa que muestra los números primos existentes entre el 1 y el 100. NOTA: Un
+		 número es primo si tiene dos únicos divisores que son el 1 y él mismo.
+		 */
+		boolean divisores = false;
+		
+		System.out.println("Números primos del 1 al 100: ");
+		for (int i = 2; i <= 100; i++) {
+			divisores = false;
+			for (int j = 2; j <= i/2 && !divisores; j++) {
+				if (i%j == 0) 
+					divisores = true;		
+			}
+			
+			if (!divisores)
+				System.out.println(i + " ");
+		}
+	}
+}
