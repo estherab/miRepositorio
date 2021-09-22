ec28e661003eb6b5a8397d36638dcad988bbed1c
esthe <esthe@DESKTOP-VTFO8S3>
2021-09-22 17:30:24 +0200

    Hoja1_ej12 Versión 2

--- Hoja1_ej12/src/paquete/Main.java
+++ Hoja1_ej12/src/paquete/Main.java
@@ -10,12 +10,13 @@ public class Main {
 		boolean divisores = false;
 		
 		System.out.println("Números primos del 1 al 100: ");
+		
 		for (int i = 2; i <= 100; i++) {
 			divisores = false;
-			for (int j = 2; j <= i/2 && !divisores; j++) {
+			
+			for (int j = 2; j <= i/2 && !divisores; j++) 
 				if (i%j == 0) 
 					divisores = true;		
-			}
 			
 			if (!divisores)
 				System.out.println(i + " ");
